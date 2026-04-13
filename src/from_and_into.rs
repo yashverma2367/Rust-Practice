936495d3c95246767d326c04f79ed714872cf548
Yash verma <yashverma2367@gmail.com>
2026-04-13 17:02:12 +0530

    From and into

---
+++ src/from_and_into.rs
@@ -0,0 +1,15 @@
+pub fn into() {
+    let s: String = "hello".into();
+    let addr: std::net::Ipv4Addr = [127, 0, 0, 1].into();
+    let one: i16 = true.into();
+    let bigger: i32 = 123_i16.into();
+    println!("{s}, {addr}, {one}, {bigger}");
+}
+
+pub fn from() {
+    let s = String::from("hello");
+    let addr = std::net::Ipv4Addr::from([127, 0, 0, 1]);
+    let one = i16::from(true);
+    let bigger = i32::from(123_i16);
+    println!("{s}, {addr}, {one}, {bigger}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
-#[derive(Debug, Copy, Clone)]
-struct Point {
-    x: i32,
-    y: i32,
-}
-
-impl std::ops::Add for Point {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Self { x: self.x + other.x, y: self.y + other.y }
-    }
-}
-
+mod from_and_into;
 fn main() {
-    let p1 = Point { x: 10, y: 20 };
-    let p2 = Point { x: 100, y: 200 };
-    println!("{p1:?} + {p2:?} = {:?}", p1 + p2);
+    from_and_into::from();
+    from_and_into::into();
 }
